41437b1601c93adb91a0ade6c5e43dc95b6b79df
TheNebulo <amir.djaffar@outlook.com>
2024-08-28 19:07:54 +0200

    "Fixed" check for updates

--- src/backend/updateManager.js
+++ src/backend/updateManager.js
@@ -1,4 +1,3 @@
-const { ipcMain } = require('electron');
 const { autoUpdater } = require("electron-updater");
 
 autoUpdater.autoDownload = false;
@@ -24,12 +23,3 @@ exports.checkForUpdates = async function () {
         return false; // Launcher is up-to-date
     }
 }
-
-ipcMain.on('check-for-updates', async (event) => { // Better feedback here
-    if (checkingForUpdates) return;
-    let updateFound = exports.checkForUpdates();
-
-    if (updateFound) {
-        // Notify update found???
-    }
-});
--- src/utils.js
+++ src/utils.js
@@ -46,69 +46,69 @@ exports.getSettingMetadata = function (key, value) {
     switch (key) {
         case 'gamesPath':
             return { 
-                "title" : "Games Folder", 
-                "desc" : "Where all games are installed",
-                "button" : "Open Folder",
-                "action" : {
-                    "type" : "open",
-                    "target" : "explorer",
-                    "path" : value,
+                title : "Games Folder", 
+                desc : "Where all games are installed",
+                button : "Open Folder",
+                action : {
+                    type : "open",
+                    target : "explorer",
+                    path : value,
                 }
             };
         case 'appVersion':
             return { 
-                "title" : "App Version", 
-                "desc" : `The current version of the app is ${value}`,
-                "button" : "Check for Updates",
-                "action" : {
-                    "type" : "ipc",
-                    "channel" : "check-for-updates",
-                    "params" : []
+                title : "App Version", 
+                desc : `The current version of the app is ${value}`,
+                button : "Check for Updates",
+                action : {
+                    type : "ipc",
+                    channel : "open-window-preset",
+                    params : ['update']
                 }
             };
         case 'betaEnabled':
             return { 
-                "title" : "Experimental Branch", 
-                "desc" : `Use the experimental build of the launcher. Currently ${ value ? "enabled" : "disabled" }`,
-                "button" : "Toggle Branch",
-                "action" : {
-                    "type" : "toggle",
-                    "title" : value ? "Return to Stable Branch" : "Enable Experimental Branch",
-                    "desc" : value ? "By returning to the stable branch, all experimental updates will be reverted and the launcher will restart." : "By enabling the experimental branch, you may be using a non-stable version of the launcher with possible bugs."
+                title : "Experimental Branch", 
+                desc : `Use the experimental build of the launcher. Currently ${ value ? "enabled" : "disabled" }`,
+                button : "Toggle Branch",
+                action : {
+                    type : "toggle",
+                    title : value ? "Return to Stable Branch" : "Enable Experimental Branch",
+                    desc : value ? "By returning to the stable branch, all experimental updates will be reverted and the launcher will restart." : "By enabling the experimental branch, you may be using a non-stable version of the launcher with possible bugs."
                 }
             };
         case 'autoUpdateEnabled':
             return { 
-                "title" : "Auto Update", 
-                "desc": `Update the launcher automatically on startup. Currently ${ value ? "enabled" : "disabled" }`,
-                "button": "Toggle Auto Update",
-                "action" : {
-                    "type" : "toggle",
-                    "title" : value ? "Disable Auto Update" : "Enable Auto Update",
-                    "desc" : value ? "By disabling auto update, all updates need to be manually downloaded by checking for updates, and critical updates can be missed." : "By enabling auto update, the launcher will automatically check for the latest launcher version and install it on startup."
+                title : "Auto Update", 
+                desc: `Update the launcher automatically on startup. Currently ${ value ? "enabled" : "disabled" }`,
+                button: "Toggle Auto Update",
+                action : {
+                    type : "toggle",
+                    title : value ? "Disable Auto Update" : "Enable Auto Update",
+                    desc : value ? "By disabling auto update, all updates need to be manually downloaded by checking for updates, and critical updates can be missed." : "By enabling auto update, the launcher will automatically check for the latest launcher version and install it on startup."
                 }
             };
         case 'updateServer':
             return { 
-                "title" : "Update Server", 
-                "desc" : "From where to download new launcher updates",
-                "button" : "Change Update Server",
-                "action" : {
-                    "type" : "input",
-                    "title" : "Change Update Server",
-                    "desc" : "By changing the update server, the app will restart and download all updates from the new URL."
+                title : "Update Server", 
+                desc : "From where to download new launcher updates",
+                button : "Change Update Server",
+                action : {
+                    type : "input",
+                    title : "Change Update Server",
+                    desc : "By changing the update server, the app will restart and download all updates from the new URL."
                 }
             };
         case 'authFailBehaviour':
             return { 
-                "title" : "On Authentication Fail", 
-                "desc" : "What to do in case your credentials can't be authenticated",
-                "button" : "Set Behaviour",
-                "action" : {
-                    "type" : "dropdown",
-                    "title" : "Authentication Fail Behaviour",
-                    "desc" : "What to do when the launcher fails to authenticate a logged-in user.",
-                    "options" : [
+                title : "On Authentication Fail", 
+                desc : "What to do in case your credentials can't be authenticated",
+                button : "Set Behaviour",
+                action : {
+                    type : "dropdown",
+                    title : "Authentication Fail Behaviour",
+                    desc : "What to do when the launcher fails to authenticate a logged-in user.",
+                    options : [
                         ["Logout and remove game files", 0],
                         ["Logout and keep game files", 1],
                         ["Do not logout", 2],
@@ -117,10 +117,10 @@ exports.getSettingMetadata = function (key, value) {
             };
         default:
             return  { 
-                "title" : `Unknown Setting: ${key}`, 
-                "desc" : "This setting is unknown and has no meta data",
-                "button" : `Value: ${value}`,
-                "action" : null
+                title : `Unknown Setting: ${key}`, 
+                desc : "This setting is unknown and has no meta data",
+                button : `Value: ${value}`,
+                action : null
             };
     }
 }
